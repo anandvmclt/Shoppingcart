7f4cd088bee832f1e0eb393010835f4d5a135a79
Anand VM <anandnambissan2@gmail.com>
2020-06-13 09:05:27 +0530

    3 First Migration Applied on Shop

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-    return render(request,'shop/index.html')
+    return render(request, 'shop/index.html')
 
-# def blog(request):
-#     return  HttpResponse("Blog Home")
+
+def about(request):
+    return HttpResponse("Shop - About page")
+
+
+def contact(request):
+    return HttpResponse("Shop - Contact page")
+
+
+def tracker(request):
+    return HttpResponse("Shop - Tracker page")
+
+
+def search(request):
+    return HttpResponse("Shop - Search page")
+
+
+def prodview(request):
+    return HttpResponse("Shop - Product page")
+
+
+def checkout(request):
+    return HttpResponse("Shop - Checkout page")
+
+
+def cart(request):
+    return HttpResponse("Shop - Cart page")
