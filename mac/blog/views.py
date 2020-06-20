2f5a314c0f14bbe485945b578c18049f2851df5c
Anand VM <anandnambissan2@gmail.com>
2020-06-20 16:05:37 +0530

    2.0 - Video#62 Finished - blog post displayed

--- mac/blog/views.py
+++ mac/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Blogpost
+# Create your views here.
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
-    return render(request,'blog/index.html')
+    mypost = Blogpost.objects.all()
+    return render(request, 'blog/index.html',{'mypost':mypost})
+
+def blogpost(request, id):
+    post = Blogpost.objects.filter(post_id = id)[0]
+    print(post)
+    return render(request, 'blog/blogpost.html',
+                  {'post':post})
